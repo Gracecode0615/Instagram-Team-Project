dac6ab46f4dcd3fdbc8ac2660d6520f1d9984869
J-Akpan <jakpan64@yahoo.com>
2025-07-29 12:12:04 +0100

    update to icon

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,11 +1,16 @@
+// import Icons from "./components/Icons";
+import IgLikes from "./pages/IgLikes";
 import InstagramAuth from "./pages/IstagramAuth";
 import "./styles/app.scss";
+import {Routes, Route} from "react-router"
 
 function App() {
   return (
     <div className="app">
-      <InstagramAuth/>
-    
+      <Routes>
+        <Route path="/login" element ={<InstagramAuth/>}></Route>
+        <Route path="/likes" element={<IgLikes/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ instagram-clone/src/pages/IgLikes.jsx
@@ -0,0 +1,14 @@
+import Icons from "../components/Icons";
+import "../styles/iglikespage.scss"
+
+const IgLikes = () => {
+    return (
+        <div className="igLikes">
+            <Icons/>
+            
+
+        </div>
+    );
+}
+ 
+export default IgLikes;
