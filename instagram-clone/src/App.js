7ed7398be28be395a1485a687b6d63efdc3b7131
AdaOkonkwo <ada.okonkwo@kodehauz.com>
2025-07-29 16:07:36 +0100

    implementing main

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,16 +1,16 @@
 import InstagramAuth from "./pages/IstagramAuth";
 import InstagramMain from './pages/instagram-main.jsx'
 import "./styles/app.scss";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink  } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Route>
-          <Routes path="/" element={<InstagramAuth/>} />
-          <Routes path="/home" element={<InstagramMain/>} />
-        </Route>
+        <InstagramAuth/>
+        <Routes>
+          <Route path="/main" element={<InstagramMain/>} />
+        </Routes>
       </BrowserRouter>
 
       
