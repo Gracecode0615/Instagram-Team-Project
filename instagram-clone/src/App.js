4cd64d3ba5dad47867cf1636d9baba2ce1d5aa19
AdaOkonkwo <ada.okonkwo@kodehauz.com>
2025-07-29 15:31:07 +0100

    intagram first main page

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,7 +1,9 @@
+import InstagramMain from './pages/instagram-main.jsx'
+
 function App() {
   return (
     <div>
-    
+      <InstagramMain />
     </div>
   );
 }
---
+++ instagram-clone/src/components/DisplayPics.jsx
@@ -0,0 +1,16 @@
+
+function DP() {
+  return (
+    <div>
+      <div className="stories">
+            <div>
+                <img src="https://via.placeholder.com/150" alt="Post 1" />
+                <div className="live"></div>
+            </div>
+            <p>Your Story</p>
+        </div>
+    </div>
+  );
+}
+
+export default DP;
