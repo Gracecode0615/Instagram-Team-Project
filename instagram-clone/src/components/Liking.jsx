04fd01d919aa22af48a79824efa26e7a833a81d7
J-Akpan <jakpan64@yahoo.com>
2025-08-01 21:32:24 +0100

    likes page update

--- instagram-clone/src/components/Liking.jsx
+++ instagram-clone/src/components/Liking.jsx
@@ -13,7 +13,7 @@ const Liking = ({profilePics, username, photo, hgt, wth, rad}) => {
             </div>
 
             <div className="username"> 
-               <p>{username} liked your photo</p>
+               <p> <span className="uname">{username}</span> liked your photo</p>
             </div>
 
              <div className="photo"> 
@@ -29,42 +29,6 @@ const Liking = ({profilePics, username, photo, hgt, wth, rad}) => {
  
 export default Liking;
 
-
-// import React from 'react';
-
-// const dummyLikesData = [
-//   {
-//     id: 1,
-//     username: 'john_doe',
-//     profilePic: 'https://via.placeholder.com/40',
-//   },
-//   {
-//     id: 2,
-//     username: 'jane_smith',
-//     profilePic: 'https://via.placeholder.com/40',
-//   },
-//   {
-//     id: 3,
-//     username: 'user123',
-//     profilePic: 'https://via.placeholder.com/40',
-//   },
-// ];
-
-// const LikesPage = () => {
-//   return (
-//     <div style={styles.container}>
-//       <h2>Likes</h2>
-//       <ul style={styles.list}>
-//         {dummyLikesData.map((like) => (
-//           <li key={like.id} style={styles.listItem}>
-//             <img src={like.profilePic} alt={like.username} style={styles.profilePic} />
-//             <span>{like.username}</span>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
 
 
 
